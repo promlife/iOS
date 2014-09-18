c6998b5a131938ae1525a7bcf1ff811d7cc2e0c4
promlife <ldcrgpf@163.com>
2014-09-18 15:26:06 +0800

    cell demo

---
+++ CellDemo/CellDemo/PFTestTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  PFTestTableViewCell.h
+//  CellDemo
+//
+//  Created by GPENGF on 14-7-16.
+//  Copyright (c) 2014年 Sylar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PFTestTableViewCell : UITableViewCell<UIAlertViewDelegate>
+
+@property (nonatomic, retain) UIButton *showBtn;
+@property (nonatomic, retain) UILabel *showLabel;
+
+@end
---
+++ CellDemo/CellDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CellDemo
+//
+//  Created by GPENGF on 14-7-16.
+//  Copyright (c) 2014年 Sylar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PFAppDelegate class]));
+    }
+}
